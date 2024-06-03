f1f07137c03b77f8d16327ac14e056a923e3cc6f
EmersonSN <emersonn956@gmail.com>
2024-06-03 17:16:38 -0300

    RamenGo: Add new models for classes and dto's

---
+++ src/main/java/org/example/dto/OrderRequest.java
@@ -0,0 +1,9 @@
+package org.example.dto;
+
+import lombok.Data;
+
+@Data
+public class OrderRequest {
+    private Long brothId;
+    private Long proteinId;
+}
---
+++ src/main/java/org/example/model/Broth.java
@@ -0,0 +1,20 @@
+package org.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Broth {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String imageInactive;
+    private String imageActive;
+    private String name;
+    private String description;
+    private int price;
+}
